c99bffc6e6f5f014d3cbb2f399a795efb22ecb86
AusCyberman <willp@Outlook.com.au>
2020-12-12 23:07:09 +1100

    cleaned up day4\n did day5

--- day4.hs
+++ day4.hs
@@ -12,41 +12,30 @@ import Debug.Trace
 import Text.Read
 import Data.Maybe
 import Data.List
-keys = M.fromList ak
-ak :: [(String, String -> Bool)]
-ak = [("cid",const True)
-        ,("byr",\x -> (read x) >= 1920 && (read x) <= 2002)
-        ,("iyr",\x -> (read x) >= 2010 && (read x) <= 2020)
-        ,("eyr",\x -> (read x) >= 2020 && (read x) <= 2030)
+keys = M.fromList $ [("cid",const True)
+        ,("byr",\x -> read x `elem` [1920..2002])
+        ,("iyr",\x -> read x `elem` [2010..2020])
+        ,("eyr",\x -> read x `elem` [2020..2030])
         ,("hgt",\x -> case break (\y -> isNothing (readMaybe [y] :: Maybe Int)) x of {
-                    (v,"cm") ->  read v >= 150 && 193 >= read v;
-                    (v,"in") ->  read v >= 59 && 76 >= read v;
-                    _ -> False;
-})
+                    (v,"cm") ->  read v `elem` [150..193];
+                    (v,"in") ->  read v `elem` [59..76];
+                    _ -> False;})
         ,("hcl", \x ->  head x == '#' 
-                    &&  length (tail x) == 6 
-                    && length ( filter (`elem` (['a'..'f'] `union` ['0'..'9']) ) (tail x )) == 6 )
-        ,("ecl",\x -> case x of {
-                    "amb" -> True;
-                    "blu" -> True;
-                    "gry" -> True;
-	            "brn" -> True;
-                    "grn" -> True;
-                    "hzl" -> True;
-                    "oth" -> True;
-		     _ -> False })
-        ,("pid",\x -> length x == 9 && (and $ map (`elem` ['0'..'9']) x))]
+                    && length (tail x) == 6 
+                    && (and $ map (`elem` (['a'..'f'] `union` ['0'..'9']) ) (tail x )))
+        ,("ecl",\x -> x `elem` ["amb","blu","gry","brn","grn","hzl","oth"])
+        ,("pid", \x -> length x == 9 && (and $ map (`elem` ['0'..'9']) x ))]
+type Passport = M.HashMap String String
+newtype Parser s a b = Parser {runParser :: (s,a) -> (s,b) }
+
 
 main = do
     f <- readFile "day4inp"
-    let (_,chunk) = runParser run (f,[])
-    print $ length chunk
+    print $ length $ run f
 
 
-
-
-prune :: Parser String [Passport] [Passport]
-prune = arr $ filter (\x ->  and $ map (flip M.member x) (filter (\a -> a/="cid") $ M.keys $ keys))  
+prune :: [Passport] -> [Passport]
+prune = filter (\x ->  and $ map (flip M.member x) (filter (\a -> a/="cid") $ M.keys $ keys))  
 
 parseKey :: String -> Maybe (String,String)
 parseKey x
@@ -54,21 +43,14 @@ parseKey x
     | otherwise = Nothing
     where inp@(key,val) = let (key,_:val) = break (==':') x in (key,val)
 
-newtype Parser s a b = Parser {runParser :: (s,a) -> (s,b) }
+
+run = prune . fold
+    where fold :: String -> [Passport]
+          fold [] = []
+          fold inp' = let (s',p) = runParser splitChunk (inp',()) in if isJust p then fromJust p : fold s' else fold s'
 
 
-run :: Parser String [Passport] [Passport]
-run = proc inp -> do
-    i <- areCharsLeft -< ()
-    if i then do 
-        inp' <- combine -< inp
-        run -< inp'
-        else do
-        prune -< inp
 
-areCharsLeft = Parser $ \(s,_) -> (s,not . null $ s)
-
-type Passport = M.HashMap String String
 
 splitChunk :: Parser String a (Maybe (M.HashMap String String))
 splitChunk = Parser $ \(s,_) -> let (rs,x) = getChunk (s,[]) in (rs,M.fromList <$> traverse parseKey (words x))
@@ -78,23 +60,4 @@ splitChunk = Parser $ \(s,_) -> let (rs,x) = getChunk (s,[]) in (rs,M.fromList <
                             (b:xss) -> getChunk (xss,xs ++ [b])
                             [] -> ([],xs)
 
-
-instance Arrow (Parser s) where
-   arr f = Parser $ Data.Bifunctor.second f
-   first (Parser f) = Parser $ \(s,(a,b)) -> let (s',b') = f (s,a) in (s',(b',b))
-instance ArrowChoice (Parser s) where
-    left (Parser f) = Parser $ \(s,a) -> case a of
-                                            Left a' -> let (s',b) = f (s,a') in (s',Left b)
-                                            Right a' -> (s,Right a')
-instance Cat.Category (Parser s) where
-    id = arr id
-    Parser f . Parser g = Parser $ f . g
-
-
-
-
-combine :: Parser String [Passport] [Passport]
-combine = Parser $ \(s,p) ->  case runParser splitChunk (s,()) of
-                                    (s', Just outp) -> (s',outp:p)
-                                    (s',Nothing) -> (s',p)
 
---
+++ day5.hs
@@ -0,0 +1,49 @@
+﻿import Debug.Trace
+data Position = F | B | L | R deriving (Show,Eq)
+type Seat = (Int,Int,Int)
+
+parseRow :: Position -> [Int] -> [Int]
+parseRow F xs  =  fst . splitAt (length xs `div` 2 ) $ xs
+parseRow B xs =   snd . splitAt (length xs `div` 2 ) $ xs
+parseRow L xs =  fst . splitAt (length xs `div` 2 ) $ xs
+parseRow R xs =   snd . splitAt (length xs `div` 2 ) $ xs
+
+traceS :: Show a => a -> a
+-- traceS = (\x -> trace (show x) x)  
+traceS = id
+main = do
+   f <- readFile "day5inp"
+   print $ mySeat f 
+mySeat f = filter (\(_,a) -> not a) $ map (\x -> (x,x`elem` ls)) alls
+	where ls = list f
+	      alls = allSeats ls
+
+list f = map (getSeatID .  parse . convertRow) $ lines f
+
+
+bignsmall xs = (head $ quickSort False xs,head $ quickSort True xs)
+
+allSeats :: [Int] -> [Int]
+allSeats xs = let (big,small) = bignsmall xs in [small..big]
+
+
+getSeatID (_,_,a) = a
+
+quickSort b (x:xs) 
+	| b = smaller' ++ [x] ++ bigger'
+	| otherwise = bigger' ++ [x] ++ smaller'
+	where smaller' = quickSort b $ filter (<x) xs
+	      bigger' = quickSort b $ filter (>=x) xs 
+quickSort b [] = []
+
+parse :: [Position] -> Seat
+parse xs = (row,col,seatID )
+	where row = head . foldl (\acc x-> parseRow x acc ) [0..127] $ traceS $ take 7 xs
+	      col = head . foldl (flip parseRow) [0..7]  $ traceS $  snd $ splitAt 7 xs 
+	      seatID = row * 8 + col
+convertRow :: String -> [Position]
+convertRow ('F':xs) = F:convertRow xs
+convertRow ('B':xs) = B:convertRow xs
+convertRow ('L':xs) = L:convertRow xs
+convertRow ('R':xs) = R:convertRow xs
+convertRow [] = []
